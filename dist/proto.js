22988b4d8ee3b8c2aec0af2000b1ed754a87d1af
MSI\Leo <engrleonelfelezario812@gmail.com>
2024-01-03 00:52:17 +0800

    add prototype chaining

--- dist/proto.js
+++ dist/proto.js
@@ -10,3 +10,10 @@ console.log(Object.getPrototypeOf(musician));
 const musicianWithPrototype = musician;
 console.log(musicianWithPrototype.plays);
 console.log(musicianWithPrototype.alive);
+const guitarist = Object.create(musician, {
+    strings: { value: 6 },
+});
+console.log(guitarist.alive);
+console.log(guitarist.plays);
+console.log(guitarist.strings);
+console.log(guitarist);
--- src/proto.ts
+++ src/proto.ts
@@ -28,5 +28,13 @@ const musicianWithPrototype: Musician & Person = musician as Musician & Person;
 console.log(musicianWithPrototype.plays); // true
 console.log(musicianWithPrototype.alive); // true
 
+// Create a guitarist object with a prototype chain using Object.create
+const guitarist: Musician & Person & { strings: number } = Object.create(musician, {
+    strings: { value: 6 },
+});
 
+console.log(guitarist.alive)
+console.log(guitarist.plays)
+console.log(guitarist.strings)
+console.log(guitarist)
 
